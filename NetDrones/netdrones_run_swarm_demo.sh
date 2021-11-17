a9bd1b3a1ecf77347c753bc5f04e096ec9a703ff
Deon Blaauw <deon@visight.tech>
2021-11-17 16:20:11 +0100

    Fixed case where no input parameters are specified

--- NetDrones/netdrones_run_swarm_demo.sh
+++ NetDrones/netdrones_run_swarm_demo.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 echo "*** NetDrones Run Swarm Demo ***
-
 Author: Deon Blaauw
-Version: v1.0"
+Version: v1.0
+********************************
+"
 
 pkill -x px4 || true
+
+if [ $# -eq 0 ]; then
+  echo "[WARNING] Please specify the number of drones to spawn e.g. ./netdrones_run_swarm_demo.sh 4 will spawn 4 drones.
+  "
+else
 
 counter=1
 while [ $counter -le $1 ]
 do
-echo $counter
-gnome-terminal -x sh -c "./run_airsim_sitl.sh $counter; bash"
-((counter++))
-done
+  echo $counter
+  gnome-terminal -x sh -c "./run_airsim_sitl.sh $counter; bash"
+  ((counter++))
+  done
+
 echo All done
 
-
+fi
 
